67b199d5367e4a4881c5fa4939a20651b58ebffc
王杰 <jie985563349@outlook.com>
2022-10-17 11:13:53 +0800

    add custom config

---
+++ custom/init.lua
@@ -0,0 +1,5 @@
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.updatetime = 300
+opt.timeoutlen = 500
---
+++ custom/plugins/overrides.lua
@@ -0,0 +1,80 @@
+local M = {}
+
+M.alpha = {
+  header = {
+    val = {
+      "                                                     ",
+      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                     ",
+    },
+  },
+}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "json",
+    "markdown",
+    "yaml",
+    "vue",
+    "tsx",
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- vim
+    "vim-language-server",
+
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "vue-language-server",
+    "eslint",
+    "pritter",
+    "json-lsp",
+
+    "yaml-language-server",
+
+    -- markdown
+    "marksman",
+    "markdownlint",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  view = {
+    width = 30,
+  },
+
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
