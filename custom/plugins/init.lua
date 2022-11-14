bc9c76d5785b3eb232fbf47307ada7e31585b7d7
王杰 <jie985563349@outlook.com>
2022-11-14 11:43:43 +0800

    add trouble plugin

--- custom/plugins/init.lua
+++ custom/plugins/init.lua
@@ -1,56 +1,63 @@
-local overrides = require("custom.plugins.overrides")
+local overrides = require "custom.plugins.overrides"
 
 return {
-	["goolord/alpha-nvim"] = {
-		disable = false,
-		override_options = overrides.alpha,
-	},
+  ["goolord/alpha-nvim"] = {
+    disable = false,
+    override_options = overrides.alpha,
+  },
 
-	["folke/which-key.nvim"] = {
-		disable = false,
-	},
+  ["folke/which-key.nvim"] = {
+    disable = false,
+  },
 
-	-- Override plugin definition options
-	["neovim/nvim-lspconfig"] = {
-		config = function()
-			require("plugins.configs.lspconfig")
-			require("custom.plugins.lspconfig")
-		end,
-	},
+  -- Override plugin definition options
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
 
-	-- overrde plugin configs
-	["nvim-treesitter/nvim-treesitter"] = {
-		override_options = overrides.treesitter,
-	},
+  -- overrde plugin configs
+  ["nvim-treesitter/nvim-treesitter"] = {
+    override_options = overrides.treesitter,
+  },
 
-	["williamboman/mason.nvim"] = {
-		override_options = overrides.mason,
-	},
+  ["williamboman/mason.nvim"] = {
+    override_options = overrides.mason,
+  },
 
-	["kyazdani42/nvim-tree.lua"] = {
-		override_options = overrides.nvimtree,
-	},
+  ["kyazdani42/nvim-tree.lua"] = {
+    override_options = overrides.nvimtree,
+  },
 
-	-- Install a plugin
-	["max397574/better-escape.nvim"] = {
-		event = "InsertEnter",
-		config = function()
-			require("better_escape").setup()
-		end,
-	},
+  -- Install a plugin
+  ["max397574/better-escape.nvim"] = {
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
 
-	-- code formatting, linting etc
-	["jose-elias-alvarez/null-ls.nvim"] = {
-		after = "nvim-lspconfig",
-		config = function()
-			require("custom.plugins.null-ls")
-		end,
-	},
+  ["folke/trouble.nvim"] = {
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup()
+    end,
+  },
 
-	["nvim-telescope/telescope.nvim"] = {
-		override_options = overrides.telescope,
-	},
+  -- code formatting, linting etc
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.null-ls"
+    end,
+  },
 
-	-- remove plugin
-	-- ["hrsh7th/cmp-path"] = false,
+  ["nvim-telescope/telescope.nvim"] = {
+    override_options = overrides.telescope,
+  },
+
+  -- remove plugin
+  -- ["hrsh7th/cmp-path"] = false,
 }
