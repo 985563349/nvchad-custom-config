c35baa2eb77c260aaabf5b7017c98d3c92025b7f
王杰 <jie985563349@outlook.com>
2022-11-25 13:41:04 +0800

    update theme

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -1,8 +1,14 @@
 local M = {}
 
+local themes = require "custom.overidden_themes"
+local highlights = require "custom.highlights"
+
 M.ui = {
   theme_toggle = { "onedark", "one_light" },
   theme = "onedark",
+  changed_themes = themes,
+  hl_override = highlights.override,
+  hl_add = highlights.add,
 }
 
 M.plugins = require "custom.plugins"
